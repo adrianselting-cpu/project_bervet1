d13b8324f1cef648fc13057f972abecc7d89d465
Adrian Selting <adrianselting@gmail.com>
2026-02-11 15:50:47 +0100

    2b

--- script_part1b.py
+++ script_part1b.py
@@ -2,20 +2,21 @@ import roadster
 import numpy as np
 import matplotlib.pyplot as plt
 
+files = ['speed_anna.npz', 'speed_elsa.npz']
 
-distance_km, speed_kmph = roadster.load_route('speed_anna.npz')
-distance_km, speed_kmph = roadster.load_route('speed_elsa.npz')
+for file in files: 
+    distance_km, speed_kmph = roadster.load_route(file)
+    s_fine = np.linspace(0, distance_km[-1], 500)
+    v_fine = roadster.velocity(s_fine, file)
 
-v1 = roadster.velocity(distance_km, 'speed_anna.npz')
-v2 = roadster.velocity(distance_km, 'speed_elsa.npz')
-v = (v1,v2)
-
-for i in v: 
+    plt.figure()
     plt.scatter(distance_km, speed_kmph)
-    plt.plot(distance_km, i)
+    plt.plot(s_fine, v_fine)
     print(distance_km)
+    plt.title(f'Rutt: {file}')
     plt.xlabel('s (km)')
     plt.ylabel('v (km/h)')
-    plt.xlim(-5, 70)
+    plt.xlim(4, 10)
     plt.ylim(0,150)
+    plt.legend()
     plt.show()
